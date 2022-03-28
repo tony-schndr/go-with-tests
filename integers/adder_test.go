508d1e46d56f54dc4d72e46c8c40eb8b67c613cb
Tony Schneider <tschneid@redhat.com>
2022-03-28 08:12:36 -0500

    integers

---
+++ integers/adder_test.go
@@ -0,0 +1,12 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("got %d but wanted %d", sum, expected)
+	}
+}
