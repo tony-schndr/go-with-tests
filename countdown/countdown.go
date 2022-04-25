6eb3e05300d6653b7485f15f3c6456ab91b3821a
Tony Schneider <tschneid@redhat.com>
2022-04-25 08:41:49 -0500

    reorganized countdown

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -13,51 +13,14 @@ type Sleeper interface {
 	Sleep()
 }
 
-type SpyCountdownOperations struct {
-	Calls []string
-}
-
-type SpySleeper struct {
-	Calls int
-}
-
-type DefaultSleeper struct {
-}
-
 type ConfigSleeper struct {
 	Duration time.Duration
 	Tsleep   func(time.Duration)
 }
 
-type SpyTime struct {
-	durationSlept time.Duration
-}
-
 func (c *ConfigSleeper) Sleep() {
 	c.Tsleep(c.Duration)
 
-}
-
-func (s *SpyTime) Sleep(duration time.Duration) {
-
-	s.durationSlept = duration
-}
-
-func (s *SpyCountdownOperations) Sleep() {
-	s.Calls = append(s.Calls, sleep)
-}
-
-func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
-	s.Calls = append(s.Calls, write)
-	return
-}
-
-func (s DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls += 1
 }
 
 func CountDown(writer io.Writer, s Sleeper) {
--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -7,6 +7,36 @@ import (
 	"time"
 )
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls += 1
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+
+	s.durationSlept = duration
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func TestCountDown(t *testing.T) {
 	buffer := &bytes.Buffer{}
 	sleeper := &SpySleeper{}
